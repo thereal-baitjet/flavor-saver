abaa02c2ac10a73570d7cbe888dbe76c1767edbd
John-Mancini <mancinijohn5988@gmail.com>
2021-05-20 18:56:01 -0400

    modals buttons working

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-// Require all the model files
-const User = require("./User");
-const Recipe = require("./Recipe");
-const Date = require("./Date");
+// // Require all the model files
+// const User = require("./User");
+// const Recipe = require("./Recipe");
+// const Date = require("./Date");
 
 // Create all the model associations
 // Each instance of a Date belongs to one User
@@ -13,22 +13,22 @@ const Date = require("./Date");
 // User.hasMany(Date, {
 //   foreignKey: "user_id",
 //   onDelete: "CASCADE",
+// // });
+
+// // One Date has many Recipes
+// Date.hasMany(Recipe, {
+//   foreignKey: "user_id",
+//   onDelete: "CASCADE",
 // });
 
-// One Date has many Recipes
-Date.hasMany(Recipe, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
-});
+// // One Recipe belongs to many Dates
+// Recipe.belongsTo(Date, {
+//   foreignKey: "user_id",
+// });
 
-// One Recipe belongs to many Dates
-Recipe.belongsTo(Date, {
-  foreignKey: "user_id",
-});
-
-// Export all the model files
-module.exports = {
-  User,
-  Recipe,
-  Date,
-};
+// // Export all the model files
+// module.exports = {
+//   User,
+//   Recipe,
+//   Date,
+// };
